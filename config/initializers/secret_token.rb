dcbef3635658ce8d8eab76247972450764341ec2
root <root@mail.gijzemijter.nl>
2014-02-26 11:14:29 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cidaron::Application.config.secret_key_base = '4909cd6bccb957f04aa8add8124407a5b4e430066b7664ff9276a9f4ad83f96a00a1c80d198fc268c9b41c486139d5daac55bd4827e0e685fc82c8e99db1fb56'
